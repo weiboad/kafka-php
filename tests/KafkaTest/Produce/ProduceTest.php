875087718fcce6ff503392015014bab08a94c89c
nmred <nmred_2008@126.com>
2014-07-08 21:47:04 +0800

    travis

--- tests/KafkaTest/Produce/ProduceTest.php
+++ tests/KafkaTest/Produce/ProduceTest.php
@@ -26,7 +26,7 @@ namespace KafkaTest\Produce;
 +------------------------------------------------------------------------------
 */
 
-class Produce extends \PHPUnit_Framework_TestCase
+class ProduceTest extends \PHPUnit_Framework_TestCase
 {
     // {{{ consts
     // }}}
--- tests/_autoload.php
+++ tests/_autoload.php
@@ -28,30 +28,29 @@ include_once __DIR__ . '/../vendor/autoload.php';
 spl_autoload_register('autoload');
 function autoload($className)
 {
-	$basePath = dirname(dirname(__FILE__)) . '/src/tests/';
-	$classFile = $basePath . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+    $basePath = dirname(dirname(__FILE__)) . '/src/tests/';
+    $classFile = $basePath . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+    var_dump($classFile);
     if (file_exists($classFile)) {
         require_once $classFile;
-        return true;    
+        return true;
     }
 //    var_dump($classFile);
-//	If (function_exists('stream_resolve_include_path')) {
-//		$file = stream_resolve_include_path($classFile);
-//	} else {
-//		foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
-//			if (file_exists($path . '/' . $classFile)) {
-//				$file = $path . '/' . $classFile;
-//				break;
-//			}
-//		}
-//	}
-//	/* If file is found, store it into the cache, classname <-> file association */
-//	if (($file !== false) && ($file !== null)) {
-//		include $file;
-//		return;
-//	}
+//    If (function_exists('stream_resolve_include_path')) {
+//        $file = stream_resolve_include_path($classFile);
+//    } else {
+//        foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
+//            if (file_exists($path . '/' . $classFile)) {
+//                $file = $path . '/' . $classFile;
+//                break;
+//            }
+//        }
+//    }
+//    /* If file is found, store it into the cache, classname <-> file association */
+//    if (($file !== false) && ($file !== null)) {
+//        include $file;
+//        return;
+//    }
 
     return false;
 }
-
-new \KafkaTest\Produce\Produce();
