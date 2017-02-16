16609821970980e633d912730d6e10389c76e80d
nmred <nmred_2008@126.com>
2017-02-16 16:43:11 +0800

    fixed

--- bin/check-cs.sh
+++ bin/check-cs.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-libraryCS=$(php php-cs-fixer.phar fix ./src --diff)
-testsCS=$(php php-cs-fixer.phar fix ./tests --diff)
+php php-cs-fixer.phar fix ./src --diff
+if [ "0" != $?"" ]; then
+    echo   -en '\E[31m'"$libraryCS
+$testsCS\033[1m\033[0m";
+    printf "\n";
+    echo   -en '\E[31;47m'"\033[1mCoding standards check failed!\033[0m"   # Red
+    printf "\n";
+    exit   2;
+fi
 
-if [[ "$libraryCS" || "$testsCS"  ]];
-then
+php php-cs-fixer.phar fix ./tests --diff
+if [ "0" != $?"" ]; then
     echo   -en '\E[31m'"$libraryCS
 $testsCS\033[1m\033[0m";
     printf "\n";
@@ -15,5 +22,3 @@ fi
 
 echo   -en '\E[32m'"\033[1mCoding standards check passed!\033[0m"   # Green
 printf "\n";
-
-echo $libraryCS$testsCS;
