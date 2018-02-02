43f5da5b3b07ea34405f1c470ad2b84eea379bd7
soul11201 <soul11201@gmail.com>
2018-02-02 22:12:15 +0800

    Revert "make  a try"
    This reverts commit 6c83884b591ab1c9fc236840b0acf87f92c8bf99.

--- src/Consumer/Process.php
+++ src/Consumer/Process.php
@@ -550,7 +550,7 @@ class Process
             foreach ($consumerOffsets as $topic => $value) {
                 foreach ($value as $partId => $offset) {
                     if (isset($lastOffsets[$topic][$partId]) && $lastOffsets[$topic][$partId] > $offset) {
-                        $consumerOffsets[$topic][$partId] = $offset + 1;
+                        $consumerOffsets[$topic][$partId] = $offset;
                     }
                 }
             }
